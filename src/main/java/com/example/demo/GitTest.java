b9b30c066c31b1be8d522c2eb013b98f5f79ce62
yuguoxiang <15684168542@163.com>
2021-07-08 14:16:35 +0800

    first commit

---
+++ src/main/java/com/example/demo/GitTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+/**
+ * @Author YuGuoxiang
+ * @Date 2021/7/8 14:09
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
